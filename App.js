e61ec4eb0760046edb3a1e777a9949f790ef0292
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-17 15:21:58 -0600

    creating App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        {/* Navbar */}
+        <Main />
+        <About />
+        <Projects />
+        <Resume />
+        <Contact />
+      </div>
+    </div>
+  );
+}
