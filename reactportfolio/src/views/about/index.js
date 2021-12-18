31c062ffe802ae25ccd2d883327fd83fafbe6690
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-18 11:41:44 -0600

    about section temp complete

--- reactportfolio/src/views/about/index.js
+++ reactportfolio/src/views/about/index.js
@@ -8,11 +8,23 @@ function About() {
         <div className="col-md about-left"></div>
       </div>
       <div className="col-md">
-          <h2 className="viewTitle" id="aboutTitle">About</h2>
-          <div className="row align-self-center bioText">
-              <p className="bio">Fill in bio text here</p>
-              <p className="bio"><span className="skills">Skill text here</span></p>
-          </div>
+        <h2 className="viewTitle" id="aboutTitle">
+          About
+        </h2>
+        <div className="row align-self-center bioText">
+          <p className="bio">Fill in bio text here</p>
+          <p className="bio">
+            <span className="skills">Skill text here</span>
+            <ul className="skillsList text-center">
+              <li className="skill">Skill</li>
+              <li className="skill">Skill</li>
+              <li className="skill">Skill</li>
+              <li className="skill">Skill</li>
+              <li className="skill">Skill</li>
+              <li className="skill">Skill</li>
+            </ul>
+          </p>
+        </div>
       </div>
     </div>
   );
