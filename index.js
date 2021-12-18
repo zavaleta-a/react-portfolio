af28c5573e9b516f55f1133408257885066834c4
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-18 10:41:52 -0600

    got the server to start

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-
-// Remember to modify this
-ReactDOM.render(<App />, document.getElementById("root"));
+// // Remember to modify this
+// ReactDOM.render(<App />, document.getElementById("root"));
--- reactportfolio/src/components/navbar/index.js
+++ reactportfolio/src/components/navbar/index.js
@@ -1,2 +1,21 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-scroll";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-md">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavMenu"
+        aria-controls="navbarNavMenu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars fa-lg"></i>
+      </button>
+    </nav>
+  );
+}
